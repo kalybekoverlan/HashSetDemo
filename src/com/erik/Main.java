8746ea227bb376d850ceccd82488cc7946595abd
erik <kalybekov.erlan@gmail.com>
2021-02-08 18:10:52 +0600

    HashSet

---
+++ src/com/erik/Main.java
@@ -0,0 +1,19 @@
+package com.erik;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashSet<String> hs=new HashSet<String>();
+        hs.add("A");
+        hs.add("B");
+        hs.add("C");
+        hs.add("D");
+        hs.add("E");
+        hs.add("F");
+        System.out.println("First print"+hs);
+
+    }
+}
